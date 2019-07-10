c8b8480e8347c57582ef62852df3435491667d70
205trinh <205trinh@gmail.com>
2019-07-10 14:31:11 +0700

    xong menu

--- 1.js
+++ 1.js
@@ -1,4 +1,14 @@
 ﻿ $(function(){
- 
+ 	vitricuaedu = $('#khoiedu').offset().top ;
+ 	console.log(vitricuaedu);
+ 	$(".xuong,.kn").click(function(){
+ 			$('body, html').animate({scrollTop:vitricuaedu},700)
+ 			return false;
+ 	})
+
+ 	$(".sp").click(function(){
+ 			$('body, html').animate({scrollTop:$('#khoisp').offset().top},700)
+ 			return false;
+ 	})
 })  
  
